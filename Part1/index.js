b5ec5e31d72e6a29ee9ebdba9f3d18289ce75fc8
Luke Hardy <lukeclaytonhardy@gmail.com>
2023-08-09 15:21:00 -0600

    working on tic-tac-toe

---
+++ Part1/index.js
@@ -0,0 +1,38 @@
+const board = Array(9).fill(null)
+
+let play = (clickedId) => {
+    const playerSpan = document.querySelector('#player')
+    const clickedElement = document.getElementById(clickedId)
+
+    clickedElement.innerText = playerSpan.innerText
+    board[clickedId] = playerSpan.innerText
+
+    const winner = calculateWinner();
+    if (winner) {
+      alert(`${winner} is the winner!`);
+    }
+    
+    if(playerSpan.innerText === 'X') {
+        playerSpan.innerText = 'O'
+    } else playerSpan.innerText = 'X'
+}
+
+function calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    for (const line of lines) {
+      const [a, b, c] = line;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+  }
